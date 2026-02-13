08abd41ca4131e28bc80891e91f95bd178079121
Alex <mntokhirjonov@gmail.com>
2026-02-13 16:27:01 +0900

    feat: publish all screen component's mobile version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { CssBaseline } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import type { AppProps } from "next/app";
 import { useState } from "react";
+import Head from "next/head";
 import "../scss/app.scss";
 import "../scss/pc/main.scss";
 import "../scss/mobile/main.scss";
@@ -12,6 +13,9 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
